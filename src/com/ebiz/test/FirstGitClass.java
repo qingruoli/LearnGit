faab87c4178317be966f705d09c26cffd40136ec
cao.qifa <cao.qifa@ebiz-interacttive.com.cn>
2017-07-25 18:09:25 +0800

    qingruoli Git branch Master Merged to Developer.

--- src/com/ebiz/test/FirstGitClass.java
+++ src/com/ebiz/test/FirstGitClass.java
@@ -4,6 +4,7 @@ public class FirstGitClass {
 
 	public static void main(String[] args) {
 		System.out.println("first Git class.");
+		System.out.println("Git branch Master Merged to Developer.");
 		return;
 	}
 }
