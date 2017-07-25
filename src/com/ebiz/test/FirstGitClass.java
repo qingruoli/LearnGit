95e132492a44e7c16bd6334e95aed5f7efc35271
cao.qifa <cao.qifa@ebiz-interacttive.com.cn>
2017-07-25 18:17:54 +0800

    Git commit to Master.

--- src/com/ebiz/test/FirstGitClass.java
+++ src/com/ebiz/test/FirstGitClass.java
@@ -5,6 +5,7 @@ public class FirstGitClass {
 	public static void main(String[] args) {
 		System.out.println("first Git class.");
 		System.out.println("Git branch Master Merged to Developer.");
+		System.out.println("Git branch Developer changes.");
 		return;
 	}
 }
